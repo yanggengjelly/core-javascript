ace2589d487ff574e4e525edb08ff58e2c982870
yanggengjelly <yangwork96@gmail.com>
2024-02-15 16:09:02 +0900

    비동기 통신 -
    delay.js
    xhr.js
    -add

--- client/main.js
+++ client/main.js
@@ -1,6 +1,10 @@
-/* eslint no-undef:off */
-//* off 는 끄는거고 warn은 경고로 바꿈
-// ? 에러를 인식해서 그 부분  eslint를 끌수있다
-//! 그리고 disable을 하면 다 끄는것
+import { xhr } from './lib/index.js';
 
-console.log("안녕 자바스크립트!");
+// xhr.post(
+//   'https://jsonplaceholder.typicode.com/users',
+//   { name: 'tiger', age: 40 },
+//   (data) => {
+//     console.log(data);
+//   },
+//   () => {}
+// );
